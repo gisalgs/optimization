ed37e9947474ec412389cf7fff8a48030641de19
Ningchuan Xiao <ncxiao@gmail.com>
2023-12-16 11:33:35 -0500

    1-center Welzl updated

--- welzl.py
+++ welzl.py
@@ -1,8 +1,8 @@
 import random
-from disc import *
 import sys
-sys.path.append('../geom')
-from point import *
+sys.path.append('..')
+from optimization.disc import *
+from geom.point import *
 
 def minidiscwith2points(P, q1, q2, D):
     D[0] = disc(points = [q1, q2])
@@ -24,25 +24,37 @@ def minidiscwithpoint(P, q, D):
             D[j] = minidiscwith2points(P[:j], P[j], q, D)
     return D[n-1]
 
-def minidisc(P):
+def minidisc(P, verbose=False):
     n = len(P)
     D = [ disc() for i in range(n)]
     D[1] = disc(points=[P[0], P[1]])
     for i in range(2, n):
+        if verbose:
+            very_simple_progress_bar(i, n)
         if D[i-1].inside(P[i]):
             D[i] = D[i-1]
         else:
             D[i] = minidiscwithpoint(P[:i], P[i], D)
     return D[n-1]
 
-def test():
-    n = 5
-    points = []
-    for i in xrange(n):
-        p = Point(random.random(), random.random())
-        points.append(p)
-    print points
-    print minidisc(points)
+
+def very_simple_progress_bar(current, upper, size=20):
+    '''
+    current    an integer indicating the current step
+    upper      an integer indicating the number of iterations
+    size       the length of the progress bar
+    
+    we assume the lower end is zero.
+    '''
+    barx = int(size*(current+1)/upper)
+    print('\r[{}{}] {}/{}'.format('x'*barx, '-'*(size-barx), current+1, upper), end='', flush=True)
+
+def test(n=5):
+    points = [Point(random.random(), random.random()) for _ in range(n)]
+    print(points)
+    res = minidisc(points, True)
+    print(res)
 
 if __name__ == '__main__':
     test()
+
